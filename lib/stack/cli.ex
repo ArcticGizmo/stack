62279c80c7d3ea8a53ea4bd9c904cb0379838f8a
ArcticGizmo <jon.howell314@gmail.com>
2021-11-06 19:14:55 +0800

    Ability to import folder data at run time

--- lib/stack/cli.ex
+++ lib/stack/cli.ex
@@ -1,52 +1,9 @@
 defmodule Stack.CLI do
-  use Stack.Trunk, "./priv/phx"
-  # @before_compile Stack.File
+  require Stack.Trunk
 
-  # defmacrop keep(path) do
-  #   IO.inspect(path)
-  #   quote do
-  #     File.read(unquote(path))
-  #   end
-  # end
+  @phx Stack.Trunk.keep("phx")
 
-  def main([]) do
-    IO.puts("--- cant do that man")
-  end
-
-  def main([name]) do
-    read_path_from_priv("phx")
-    |> dump_structure(name)
-  end
-
-  defp read_path_from_priv(priv_path) do
-    rel_path = "./priv/#{priv_path}"
-
-    cull_path = "priv/#{priv_path}/"
-
-    paths =
-      Path.wildcard("#{rel_path}/**/*", match_dot: true)
-      |> IO.inspect()
-
-    paths
-    |> Enum.map(fn path ->
-      short_path = String.replace(path, cull_path, "", global: false)
-
-      case File.dir?(path) do
-        true -> nil
-        false -> {short_path, File.read!(path)}
-      end
-    end)
-    |> Enum.reject(&is_nil/1)
-    |> Enum.into(%{})
-  end
-
-  defp dump_structure(structure, dirname) do
-    target = Path.join(File.cwd!(), dirname)
-
-    Enum.each(structure, fn {short_path, data} ->
-      path = Path.join(target, short_path)
-      File.mkdir_p!(Path.dirname(path))
-      File.write(path, data)
-    end)
+  def main(_args) do
+    IO.inspect(@phx)
   end
 end
--- lib/stack/trunk.ex
+++ lib/stack/trunk.ex
@@ -1,9 +1,39 @@
 defmodule Stack.Trunk do
-  defmacro __using__(path) when is_binary(path) do
-    file = File.read!("./priv/phx/sample.ex")
+  defmacro keep(priv_path) do
+    structure = read_path_from_priv(priv_path)
 
     quote do
-      Module.put_attribute(__MODULE__, :files, unquote(file))
+      unquote(structure)
+      |> Enum.into(%{})
     end
+  end
+
+  defp read_path_from_priv(priv_path) do
+    rel_path = "./priv/#{priv_path}"
+
+    cull_path = "priv/#{priv_path}/"
+
+    paths = Path.wildcard("#{rel_path}/**/*", match_dot: true)
+
+    paths
+    |> Enum.map(fn path ->
+      short_path = String.replace(path, cull_path, "", global: false)
+
+      case File.dir?(path) do
+        true -> nil
+        false -> {short_path, File.read!(path)}
+      end
+    end)
+    |> Enum.reject(&is_nil/1)
+  end
+
+  def dump_structure(structure, dirname) do
+    target = Path.join(File.cwd!(), dirname)
+
+    Enum.each(structure, fn {short_path, data} ->
+      path = Path.join(target, short_path)
+      File.mkdir_p!(Path.dirname(path))
+      File.write(path, data)
+    end)
   end
 end
